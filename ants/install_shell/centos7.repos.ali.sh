97e0ec18137267b19830e605631ad1c6af3102b0
dinghao2020 <18001326539@163.com>
2017-10-31 09:30:48 +0800

    c7 ali

---
+++ ants/install_shell/centos7.repos.ali.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+yum clean all && yum makecache
