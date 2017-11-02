2914a03626aca4e4f790b258527885aa64b8b2e8
dinghao2020 <18001326539@163.com>
2017-11-02 09:31:31 +0800

    swapoff

--- ants/install_shell/install_k8s.sh
+++ ants/install_shell/install_k8s.sh
@@ -10,7 +10,10 @@ swapoff -a
 cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+sysctl vm.swappiness=0
 EOF
+
+sysctl vm.swappiness=0
 
 sysctl --system
 systemctl stop firewalld && systemctl disable firewalld
